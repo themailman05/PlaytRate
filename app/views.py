889dc7838d99700a130a9788a3c8805e65b2d044
Liam Sargent <themailman05@gmail.com>
2015-02-06 23:08:07 -0500

    more flask

--- app/views.py
+++ app/views.py
@@ -1,16 +1,21 @@
 from flask import render_template
+from flask import jsonify
+from flask import request
 from app import app
 
 @app.route('/')
 @app.route('/index')
 @app.route('/search')
+@app.route("/getip", methods=["GET"])
+
+
 
 def index():
-    user = {'nickname': 'Miguel'}
     return render_template('index.html',
-                           title='Home',
-                           user=user)
+                           title='Home',)
 
+def get_my_ip():
+    return jsonify({'ip': request.environ['REMOTE_ADDR']}), 200
 
 def search():
     return "You have reached the search page."
