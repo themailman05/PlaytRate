0f3122f542ae1d2b3d959ee883bcf2594f42dc9c
David McVicar <dwmcvicar@gmail.com>
2015-02-07 08:36:36 -0500

    analysis.py is almost there

--- app/apis/analyzer.py
+++ app/apis/analyzer.py
@@ -1,12 +1,15 @@
 """
 Sentiment Analyzer that reads twitter feeds
 
+
 @author David McVicar
 """
 
 import tweepy
 import urllib
-import oauth2
+import urllib2
+import json
+from bs4 import BeautifulSoup
 
 import yelp_api
 import yellow_api
@@ -18,7 +21,7 @@ T_CONSUMER_SECRET = '5a6BAdhFEuW3MBAt5AFdiaZwN7gyrk6TkaCLmmV6B5ydtzBoWv'
 T_ACCESS_TOKEN = '54745215-vsvjElxhcznr0HTmpnJ3giGC3VkkiZFTjHI480hvY'
 T_ACCESS_SECRET = '5ZdfmM2YxKEGCjmvOFGKWq5Sz6YATr1V7zbI8cYDWja48'
 
-T_WEB_SEARCH_URL = 'https://twitter.com/search'
+T_WEB_SEARCH_URL = 'https://www.twitter.com/search/?q='
 
 
 def analyze(name, location):
@@ -30,17 +33,28 @@ def analyze(name, location):
 
    place_id = api.reverse_geocode(location.get('lat'),location.get('long'))[0].id
 
-   #tweets = api.search(q = 'place%3A'+place_id+'%20%22'+urllib.quote(name)+'%22')
-   tweets = api.search(q = 'Pizza')
-   print 'place%3A' + place_id + '%20%22' + urllib.quote(name) + '%22'
-   print tweets
-
-   alchy = alchemyapi.AlchemyAPI()
-   #final_result = alchy.sentiment_targeted('url',search_url,name)
+   search_url = T_WEB_SEARCH_URL + 'place%3A'+place_id+'%20%22'+urllib.quote(name)+'%22'
    
+   page = urllib2.urlopen(search_url)
+   html = page.read()   
+
+   soup = BeautifulSoup(html)
+   tweets = soup.find_all('p','js-tweet-text')
+   tweet_texts = ""
+   for i in range(len(tweets)):
+      tweet_texts = tweet_texts + tweets[i].get_text().encode('ascii', 'ignore') + '\n'
+   
+   alchy = alchemyapi.AlchemyAPI()
+   final_result = alchy.sentiment_targeted('text',tweet_texts,name)
+
+   print tweet_texts
+   print final_result
+
+   #print final_result['docSentiment']['type']
+   #print final_result['docSentiment']['score']
 
 def main():
-   analyze("Starbucks",{ 'lat' : 37.2304516, 'long' : -80.4294548})
+   analyze("Sbarro's",{ 'lat' : 40.722196, 'long' : -73.987429})
 
 
 if __name__ == "__main__":
