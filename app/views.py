ef2e58a9ef94f76a0b1bdb114c540c3a70989017
David McVicar <dwmcvicar@gmail.com>
2015-02-08 05:19:12 -0500

    Fixed this class

--- app/views.py
+++ app/views.py
@@ -57,11 +57,7 @@ def analyze():
     name = request.args['name']
     businessinfo = yelp_api.getBusinessDetail(name)
     #print "BUSINESSINFO: " +str(businessinfo)
-<<<<<<< HEAD
     location = { 'lat':businessinfo['location']['coordinate']['latitude'],'long':businessinfo['location']['coordinate']['longitude'] }
-=======
-    location = {'lat':businessinfo['location']['coordinate']['latitude'],'long':businessinfo['location']['coordinate']['longitude']}
->>>>>>> 316048a80c242ef36986798cc493c410f82e2c59
     readablename = businessinfo['name']
     yelpstars = businessinfo['rating']
     reviewcount = businessinfo['review_count']
